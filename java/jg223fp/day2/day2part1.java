acc8f8a3e6404d9c6229ce12cfbc75a5a45a6ec7
jg223fp <jg223fp@student.lnu.se>
2022-12-02 07:27:59 +0100

    part1 done

---
+++ java/jg223fp/day2/day2part1.java
@@ -0,0 +1,74 @@
+package day2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day2part1 {
+
+    public static void main(String[] args) throws FileNotFoundException{
+      File file = new File("C:\\src\\github\\advent-of-code-2022\\java\\jg223fp\\day2\\input.txt");
+      Scanner scan = new Scanner(file);
+
+      int total = 0;
+
+      while (scan.hasNextLine()){
+        try {
+          String[] round = scan.nextLine().split(" ");
+          switch (round[0]) {
+            case "A":
+              switch (round[1]) {
+                case "X":   
+                  total += 4;
+                  break;
+                case "Y":
+                  total += 8;   
+                  break;
+                case "Z":
+                  total += 3;
+                  break;
+                default:
+                  break;
+              } 
+              break;
+            case "B":
+              switch (round[1]) {
+                case "X":
+                  total += 1;
+                  break;
+                case "Y":
+                  total += 5;
+                  break;
+                case "Z":
+                  total += 9;
+                  break;
+                default:
+                  break;
+              }
+              break;
+            case "C":
+              switch (round[1]) {
+                case "X":
+                  total += 7;
+                  break;
+                case "Y":
+                  total += 2; 
+                  break;
+                case "Z":
+                  total += 6;
+                  break;
+                default:
+                  break;
+              }
+              break;
+          
+            default:
+              break;
+          }
+        } catch (Exception e) { 
+        }
+      }
+    System.out.println(total);
+    scan.close();
+    }
+  }
